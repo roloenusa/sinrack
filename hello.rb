b2cbc6f8a98eb90461aa593eb42ff193a334888e
Juan Delgado <juan.delgado@wbgames.com>
2019-03-14 13:17:19 -0700

    Expose environment settings

--- hello.rb
+++ hello.rb
@@ -1,5 +1,40 @@
+require 'json'
+require 'logger'
 require 'sinatra'
+
+before do
+  request.body.rewind
+  @body = {}
+  begin
+    @body = JSON.parse request.body.read
+  rescue
+    @body = "NO BODY"
+  end
+end
 
 get '/' do
   "Hello World!"
+end
+
+get '/expose' do
+  output = {
+    'Headers' => request.env,
+    'Params' => params
+  }
+  output = JSON.pretty_generate(output)
+  logger.info(output)
+  
+  output
+end
+
+post '/expose' do
+  output = {
+    'ENV' => request.env,
+    'Params' => params,
+    'Body' => @body
+  }
+  output = JSON.pretty_generate(output)
+  logger.info(output)
+
+  output
 end
